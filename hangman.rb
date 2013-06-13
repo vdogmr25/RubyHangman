b31b3dadd6374aba31a48d66c08d58bef61986ec
vdogmr25 <cbodnar25@gmail.com>
2013-06-13 10:10:25 -0700

    Comments the code and adds single letter input and letter check against the word.

--- hangman.rb
+++ hangman.rb
@@ -1,3 +1,13 @@
-#dictionary = IO.readlines('dictionary.txt')
-#puts dictionary.sample
-puts IO.readlines('dictionary.txt').sample
+#grabs random dictionary word
+word = IO.readlines('dictionary.txt').sample
+
+guess = gets
+guess.chomp!
+while (guess.size != 1)
+    puts 'Invalid guess.  Please try again.'
+    guess = gets
+    guess.chomp!
+end
+
+puts word
+puts word.include? guess
