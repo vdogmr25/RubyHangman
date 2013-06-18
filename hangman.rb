d3fd1caa2185220d2ff0526a9780ece7553a41a6
vdogmr25 <cbodnar25@gmail.com>
2013-06-18 09:09:54 -0700

    Improved error trap so that only letters get through and an incorrect guess tracker.

--- hangman.rb
+++ hangman.rb
@@ -3,27 +3,34 @@ word = IO.readlines('dictionary.txt').sample
 word.chomp!
 word.downcase!
 
-#Correct guesses string
+#Guesses strings
 correctGuesses = ""
+wrongGuesses = ""
 
 #gets user input
+print "Please make a guess: "
 guess = gets
 guess.chomp!
+guess.downcase!
 
 #makes sure the input is only one letter
-while (guess.size != 1)
-    puts 'Invalid guess.  Please try again.'
+while !(/^[a-z]$/.match(guess))
+    puts "Invalid guess.  Please try again."
+    print "Please make a guess: "
     guess = gets
     guess.chomp!
+    guess.downcase!
 end
 
 # Displays the word and if the letter is in the word for debug
 puts word
 puts word.include? guess
 
-# Adds to correct letter list
+# Adds the guess to the proper letter list
 if word.include? guess
     correctGuesses << guess
+else
+    wrongGuesses << guess
 end
 
 # Iterates over the word and displays correct guesses.
@@ -37,4 +44,11 @@ word.split("").each do |i|
 end
 
 # Endline
+print "\n"
+
+# Prints the wrong guesses
+print "Incorrect Guesses: "
+wrongGuesses.split("").each do |i|
+    print i + " "
+end
 print "\n"
