092011918aacef49f66e03753e00387f7449ca43
vdogmr25 <cbodnar25@gmail.com>
2013-06-18 09:23:57 -0700

    Added loop.  Exits on win or when there are 5 wrong guesses.

--- hangman.rb
+++ hangman.rb
@@ -7,48 +7,57 @@ word.downcase!
 correctGuesses = ""
 wrongGuesses = ""
 
-#gets user input
-print "Please make a guess: "
-guess = gets
-guess.chomp!
-guess.downcase!
+#Win check
+win = false
 
-#makes sure the input is only one letter
-while !(/^[a-z]$/.match(guess))
-    puts "Invalid guess.  Please try again."
+#Loops until game is done.
+while !win && !(wrongGuesses.size > 4)
+
+    #gets user input
     print "Please make a guess: "
     guess = gets
     guess.chomp!
     guess.downcase!
-end
 
-# Displays the word and if the letter is in the word for debug
-puts word
-puts word.include? guess
-
-# Adds the guess to the proper letter list
-if word.include? guess
-    correctGuesses << guess
-else
-    wrongGuesses << guess
-end
-
-# Iterates over the word and displays correct guesses.
-word.split("").each do |i|
-    if correctGuesses.include? i
-        print i
-    else
-        print "_"
+    #makes sure the input is only one letter
+    while !(/^[a-z]$/.match(guess))
+        puts "Invalid guess.  Please try again."
+        print "Please make a guess: "
+        guess = gets
+        guess.chomp!
+        guess.downcase!
     end
-    print " "
-end
 
-# Endline
-print "\n"
+    # Displays the word and if the letter is in the word for debug
+    puts word
+    puts word.include? guess
 
-# Prints the wrong guesses
-print "Incorrect Guesses: "
-wrongGuesses.split("").each do |i|
-    print i + " "
+    # Adds the guess to the proper letter list
+    if word.include? guess
+        correctGuesses << guess
+    else
+        wrongGuesses << guess
+    end
+
+    # Iterates over the word and displays correct guesses.
+    win = true
+    word.split("").each do |i|
+        if correctGuesses.include? i
+            print i
+        else
+            print "_"
+            win = false
+        end
+        print " "
+    end
+
+    # Endline
+    print "\n"
+
+    # Prints the wrong guesses
+    print "Incorrect Guesses: "
+    wrongGuesses.split("").each do |i|
+        print i + " "
+    end
+    print "\n"
 end
-print "\n"
